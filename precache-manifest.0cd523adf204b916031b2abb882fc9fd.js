9966145e99030e1bc92e566e3dc5482d90b3675f
IlkaiGundogun <piccasoul19@ukr.net>
2020-08-11 17:37:40 +0300

    Updates

---
+++ precache-manifest.0cd523adf204b916031b2abb882fc9fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4473e3b2a4e02c332c7641543eb974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03d9805c8138c1f3410e",
+    "url": "/static/css/main.5a3f462d.chunk.css"
+  },
+  {
+    "revision": "78b681b5d2ea438bfa89",
+    "url": "/static/js/2.7050f82a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7050f82a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d9805c8138c1f3410e",
+    "url": "/static/js/main.781c9167.chunk.js"
+  },
+  {
+    "revision": "4ff1f0d5bce29dab4e96",
+    "url": "/static/js/runtime-main.3ce725fe.js"
+  }
+]);
